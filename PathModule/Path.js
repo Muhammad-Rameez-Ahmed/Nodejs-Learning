020ac50a218b1a2f92a70d665744802b7944acef
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-22 12:21:39 +0500

    Step9 Path Module

---
+++ PathModule/Path.js
@@ -0,0 +1,10 @@
+const  path= require("path");
+
+
+console.log(path.dirname("E:/Nodejs Learning/PathModule/Path.js"))
+
+console.log(path.basename("E:/Nodejs Learning/PathModule/Path.js"))
+
+console.log(path.extname("E:/Nodejs Learning/PathModule/Path.js"))
+
+console.log(path.parse("E:/Nodejs Learning/PathModule/Path.js"))
