4f2b3fdd52cb24b5c943070f16c6591dc0d1d985
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-22 16:52:07 +0500

    Step11 npm init, npm module download chalk,validator

---
+++ npmModule/index.js
@@ -0,0 +1,19 @@
+// import chalk from 'chalk'
+
+// console.log(chalk.green.underline('false'));
+
+// const error = chalk.bold.red;
+// const warning = chalk.hex('#FFA500'); // Orange color
+
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
+
+
+
+// const name = 'Sindre';
+// console.log(chalk.green('Hello %s'), name);
+import validator from 'validator';
+// var validator = require('validator');
+
+ console.log(validator.isEmail('ram@zz.com')); 
+
