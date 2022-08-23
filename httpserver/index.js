15b62a29d40ac1a53359791ed2c2c95deee62763
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-24 01:39:06 +0500

    step13 http request with routing concept got url ,import url module

--- httpserver/index.js
+++ httpserver/index.js
@@ -3,10 +3,22 @@ const http = require("http");
 
 
 const server=http.createServer((req,res)=>{
-    res.end('hello from other side');
+//   console.log(  req.url);
+
+if(req.url =='/'){
+    res.end('hello from the home page');
+}else if(req.url == '/about'){
+    res.end("hello  from about page")
+}else if(req.url == '/contact'){
+    
+    res.end("hello from contact page")
+}else{
+    res.writeHead(404,{"content-type":"text/html" })
+    res.end("404 error page ")
+}
 })
 
 server.listen(9000,"127.0.0.1",()=>{
-    console.log("listening tje port number 9000")
+    console.log("listening to port number 9000")
 })
  
